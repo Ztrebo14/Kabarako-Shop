ef3db6f5fde75d4fead3d14c8b1f2b09d1ae3015
Ztrebo14 <robertabon0414@gmail.com>
2024-06-09 22:58:08 +0800

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkICeiEWTYs2ohzo9jdyIRI95rlRvZsf0",
+  authDomain: "kabarako-shop.firebaseapp.com",
+  projectId: "kabarako-shop",
+  storageBucket: "kabarako-shop.appspot.com",
+  messagingSenderId: "24565015206",
+  appId: "1:24565015206:web:51fc04d8be69e0a3c00a67",
+  measurementId: "G-5SSTVGMXZF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+// Detect auth state
+onAuthStateChanged(auth, user => {
+  if(user != null) {
+    console.log('Logged in!')
+  } else {
+    console.log('No user')
+  }
+})
